8df0aeca7efc68b51a5cd1267ac56d697ac5f052
Ventura <ventura.py3@gmail.com>
2024-07-26 22:32:21 -0300

    First and last commit

---
+++ Action/Program.cs
@@ -0,0 +1,36 @@
+﻿using Actions.Entities;
+
+namespace Actions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            // Chamando o método action direto
+            list.ForEach(UpdatePrice);
+
+            // instanciando um action, atribuindo o método a ele e chamando a instancia
+            Action<Product> action = UpdatePrice;
+            list.ForEach(action);
+
+            // atribuindo uma expressão lambda à instancia do action
+            Action<Product> action2 = p => { p.Price += p.Price * 0.1; };
+            list.ForEach(action2);
+
+            // Colocando a expressão inline no método Foreach
+            list.ForEach(p => { p.Price += p.Price * 0.1; });
+
+        }
+
+        static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,37 @@
+﻿using Delegates.Services;
+
+namespace Delegates
+{
+    // Um delegate que referencia qualquer método que receba dois double
+    // como parâmetros e retorne um double
+    delegate double BinaryNumericOperation(double n1, double n2);
+
+    // Multicast
+    delegate void BinaryNumericOperationMultiCast(double n1, double n2);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Max;
+            BinaryNumericOperation op2 = new BinaryNumericOperation(CalculationService.Sum);
+
+            // Multicast
+            BinaryNumericOperationMultiCast opmc = CalculationService.ShowSum;
+            opmc += CalculationService.ShowMax;
+
+            double result = op(a, b);
+            double result2 = op2.Invoke(a, b);
+            double result3 = CalculationService.Square(b);
+
+            /*Console.WriteLine(result);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);*/
+
+            opmc(a, b);
+        }
+    }
+}
